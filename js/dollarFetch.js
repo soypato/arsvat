b1d139b1e4e339df1cb4247f4f965a6fa7589f1d
soypato <patriciotubio@gmail.com>
2023-10-15 01:53:17 -0300

    change iva: now is 100% :(

--- js/dollarFetch.js
+++ js/dollarFetch.js
@@ -4,15 +4,15 @@ const dollarNow = document.querySelector("#dollarNow"),
   inputPrice = document.querySelector("#inputPrice"),
   currency = document.querySelector('#currency'),
   writingInput = document.querySelector("#writingInput"),
-  iva8Span = document.querySelector("#iva8Span"),
+  iva25Span = document.querySelector("#iva25Span"),
   iva30Span = document.querySelector("#iva30Span"),
-  iva35Span = document.querySelector("#iva35Span"),
+  iva45Span = document.querySelector("#iva45Span"),
   arsValue = document.querySelector("#arsValue"),
   totalIva = document.querySelector("#totalIva"),
   total = document.querySelector("#total"),
   printButton = document.querySelector('#printButton');
 
-let dollarPrice, dollarBluePrice, euroPrice, btcPrice, ethPrice, bnbPrice, btcData, ethData, bnbData, newPrice, typeCurrency, iva8Value, iva30Value, iva35Value, totalIvaValue, totalValue;
+let dollarPrice, dollarBluePrice, euroPrice, btcPrice, ethPrice, bnbPrice, btcData, ethData, bnbData, newPrice, typeCurrency, iva25Value, iva30Value, iva45Value, totalIvaValue, totalValue;
 
 let variableDollar;
 
@@ -110,19 +110,19 @@ formDollar.addEventListener("submit", async (e) => {
 
   arsValue.innerHTML = `ARS ${newPrice.toLocaleString('es-AR')}`;
 
-  iva8Value = Number((newPrice * 0.08))
+  iva25Value = Number((newPrice * 0.25))
   iva30Value = Number((newPrice * 0.3));
-  iva35Value = Number((newPrice * 0.35));
+  iva45Value = Number((newPrice * 0.45));
 
   if (typeCurrency === 'fiat') {
-    iva8Span.innerHTML = `ARS ${iva8Value.toLocaleString('es-AR')}`;
+    iva25Span.innerHTML = `ARS ${iva25Value.toLocaleString('es-AR')}`;
     iva30Span.innerHTML = `ARS ${iva30Value.toLocaleString('es-AR')}`;
-    iva35Span.innerHTML = `ARS ${iva35Value.toLocaleString('es-AR')}`;
-    totalIvaValue = Number((iva8Value + iva30Value + iva35Value).toFixed(2));
+    iva45Span.innerHTML = `ARS ${iva45Value.toLocaleString('es-AR')}`;
+    totalIvaValue = Number((iva25Value + iva30Value + iva45Value).toFixed(2));
   } else if (typeCurrency === 'crypto' || typeCurrency === 'blue') {
-    iva8Span.innerHTML = 'ARS 0';
+    iva25Span.innerHTML = 'ARS 0';
     iva30Span.innerHTML = 'ARS 0';
-    iva35Span.innerHTML = 'ARS 0';
+    iva45Span.innerHTML = 'ARS 0';
     totalIvaValue = 0;
   }
   totalIva.innerHTML = `ARS ${totalIvaValue.toLocaleString('es-AR')}`;
@@ -134,9 +134,9 @@ formDollar.addEventListener("submit", async (e) => {
 inputPrice.addEventListener('input', () => {
   if (inputPrice.value === '') {
     arsValue.innerHTML = '';
-    iva8Span.innerHTML = ''
+    iva25Span.innerHTML = ''
     iva30Span.innerHTML = ''
-    iva35Span.innerHTML = ''
+    iva45Span.innerHTML = ''
     totalIva.innerHTML = '';
     total.innerHTML = `ARS 0`;
   }
